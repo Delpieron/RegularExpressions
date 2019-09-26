e8aa03bca36f5016a32b5142f333e5f20b86a116
Krzysztof Wróblewski <k.wroblewski@microsoft.wsei.edu.pl>
2019-09-26 20:15:58 +0200

    Add project files.

---
+++ RegularExpressions/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegularExpressions
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string text = Console.ReadLine();
+                bool isOK = Regex.IsMatch(text, "^([[:alnum:]])*([0-9.])*[[:alnum:]]*+@([[:alnum:]])*+[0-9.]{1}([[:alnum:]])*");
+                Console.WriteLine(isOK);
+                Console.Read();
+            }
+        }
+    }
+}
